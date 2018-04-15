f7ba139806f0a68a714865adf01d293bb15bbc09
Kirill Taran <kirill.t256@gmail.com>
2018-04-15 19:57:25 +0300

    Automated test: verification of construction & paths to nodes

--- src/lib.rs
+++ src/lib.rs
@@ -12,34 +12,22 @@ mod tests {
     use std::iter::FromIterator;
 
     #[test]
-    fn merkle_tree_is_valid_for_any_size() {
+    fn automated_test() {
         for n in 0..100 {
-            let range = (1..n + 1).collect::<Vec<usize>>();
-            let txs = range.iter().map(|i| format!("tx{}", i));
-            let merkle = Merkle::from_iter(txs);
+            let merkle = tree_with_n_leaves(n);
             assert!(merkle.verify_tree());
+
+            assert!(merkle.path("absent_key").is_none());
+            for key in merkle.data.keys() {
+                let path = merkle.path(key).unwrap();
+                assert!(merkle.verify_path(key, &path));
+            }
         }
     }
 
-    #[test]
-    fn it_works() { //todo
-        let txs = vec!["tx1", "tx2", "tx3", "tx4", "tx5", "tx6", "tx7"];
-
-        let merkle_tree = Merkle::from_iter(txs);
-
-        println!("Data: {:?}", merkle_tree.data);
-        println!("Tree: {:?}", merkle_tree.tree);
-        println!("Verification passed? {}", merkle_tree.verify_tree());
-        println!();
-
-        for key in merkle_tree.data.keys() {
-            let path = merkle_tree.path(key);
-            println!("Path of {} is {:?}", key, path);
-            println!(
-                "Verification passed? {}",
-                merkle_tree.verify_path(key, &path.unwrap())
-            );
-            println!();
-        }
+    fn tree_with_n_leaves(n: usize) -> Merkle {
+        let range = (1..n + 1).collect::<Vec<usize>>();
+        let txs = range.iter().map(|i| format!("tx{}", i));
+        Merkle::from_iter(txs)
     }
 }
